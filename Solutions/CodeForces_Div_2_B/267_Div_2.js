69d2fc8b07d36d5cb8f3466bd24c519c8c7c19ed
unknown <sayooj0076@gmail.com>
2023-09-20 01:12:45 +0530

    7:40

---
+++ Solutions/CodeForces_Div_2_B/267_Div_2.js
@@ -0,0 +1,28 @@
+let n = 7;
+let m = 3;
+let k = 1;
+let soliders = [8, 5, 111, 17];
+let binnary = [];
+for (let i = 0; i <= m; i++) {
+  binnary.push(soliders[i].toString(2));
+}
+console.log(binnary);
+let friends = 0;
+let pointer = m;
+
+for (let i = 0; i < binnary.length - 1; i++) {
+  let count = 0;
+  for (let j = 0; j < binnary[i].length; j++) {
+    console.log("bt =",binnary[i] ^ binnary[pointer]);
+    if (binnary[i][j] !== binnary[pointer][j]) {
+      count++;
+    }
+  }
+  count = count + Math.abs(binnary[i].length - binnary[pointer].length);
+  console.log(count);
+  if (count <= k) {
+    friends++;
+  }
+}
+
+console.log(friends);
--- Solutions/CodeForces_Div_2_B/Template.js
+++ Solutions/CodeForces_Div_2_B/Template.js
@@ -30,25 +30,31 @@ function main() {
     .map((x) => parseInt(x));
   let n = temp[0];
   let m = temp[1];
-  let houseArray = readline()
-    .replace(/\r/g, "")
-    .split(" ")
-    .map((x) => parseInt(x));
-  let currentHouse = 1;
-  let pointer = 0;
+  let k = temp[2];
+  let soliders = [];
+  for (let i = 0; i < m; i++) {
+    let temp = readline().replace(/\n/g, "");
+    soliders.push(temp);
+  }
+  let binnary = [];
+  for (let i = 0; i <= m; i++) {
+    binnary.push(soliders[i].toString(2));
+  }
+  let friends = 0;
+  let pointer = m;
 
-  let time = 0;
-  while (pointer < m) {
-    time = time + Math.abs(currentHouse - houseArray[pointer]);
-    currentHouse = houseArray[pointer];
-    pointer++;
-    if (houseArray[pointer] < currentHouse) {
-      time = time + Math.abs(currentHouse - n);
-      currentHouse = 1;
-    } else {
-      time = time + Math.abs(currentHouse - houseArray[pointer]);
-      pointer++;
+  for (let i = 0; i < binnary.length - 1; i++) {
+    let count = 0;
+    for (let j = 0; j < binnary[i].length; j++) {
+      if (binnary[i][j] !== binnary[pointer][j]) {
+        count++;
+      }
+    }
+    count = count + Math.abs(binnary[i].length - binnary[pointer].length);
+    if (count <= k) {
+      friends++;
     }
   }
-  console.log(time + 1);
+
+  console.log(friends);
 }
