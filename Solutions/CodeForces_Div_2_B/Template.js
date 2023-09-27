ea7f604c2f8f7ec2a154861d56c48c904a055f63
unknown <sayooj0076@gmail.com>
2023-09-27 23:16:48 +0530

    11:16

--- Solutions/CodeForces_Div_2_B/Template.js
+++ Solutions/CodeForces_Div_2_B/Template.js
@@ -22,29 +22,20 @@ function readline() {
 // ********** Code Start **********
 
 function main() {
-  // Number of lanterns and length of the street
+
   let [n, l] = readline().split(" ").map(Number);
-
-  // Positions of lanterns
   let lanterns = readline().split(" ").map(Number);
-
-  // Sort the lantern positions in ascending order
   lanterns.sort((a, b) => a - b);
-
-  // Initialize the maximum distance to the first lantern position
   let maxDistance = lanterns[0];
 
-  // Iterate through the lantern positions and calculate the maximum gap
   for (let i = 1; i < n; i++) {
     let gap = (lanterns[i] - lanterns[i - 1]) / 2;
     maxDistance = Math.max(maxDistance, gap);
   }
 
-  // Consider the distance to the last lantern and the street ends
   maxDistance = Math.max(maxDistance, l - lanterns[n - 1]);
 
-  // Output the result with 10 decimal places
   console.log(maxDistance.toFixed(10));
 }
 
-// ********** Code End **********
+
