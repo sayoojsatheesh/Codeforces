5ac9888a70331d776ed4938a96408a17bafa915f
unknown <sayooj0076@gmail.com>
2023-10-31 00:50:05 +0530

    12:50

--- Solutions/CodeForces_Div_2_B/246_Div_2.js
+++ Solutions/CodeForces_Div_2_B/246_Div_2.js
@@ -4,22 +4,33 @@ let teams = [
   [2, 1],
   [1, 3],
 ];
-let count = [];
+var rows = n;
+var columns = 2;
+
+// Create a 2D array with inner arrays pre-filled with 0, 0
+var count = new Array(rows);
+
+for (var i = 0; i < rows; i++) {
+  count[i] = new Array(columns).fill(0);
+}
+
 let pointer = 0;
+let home = n - 1;
 while (pointer < n) {
-  let home = n - 1;
-  let away = 0;
-  for (let i = 0; i < n; i++) {
-    if (pointer == i) {
-      continue;
+  for (let i = pointer + 1; i < n; i++) {
+    if (teams[pointer][1] == teams[i][0]) {
+      count[pointer][0]++;
     } else {
-      if (teams[pointer][1] == teams[i][0]) {
-        home++;
-      } else {
-        away++;
-      }
+      count[pointer][1]++;
+    }
+
+    if (teams[pointer][0] == teams[i][1]) {
+      count[i][0]++;
+    } else {
+      count[i][1]++;
     }
   }
- console.log(`${home} ${away}`)
+  count[pointer][0] = home + count[pointer][0];
+  console.log(`${count[pointer][0]} ${count[pointer][1]}`);
   pointer++;
 }
--- Solutions/CodeForces_Div_2_B/Template.js
+++ Solutions/CodeForces_Div_2_B/Template.js
@@ -31,23 +31,36 @@ function main() {
       .map((x) => parseInt(x));
     teams.push(temp);
   }
-  let pointer = 0;
-  while (pointer < n) {
-    let home = n - 1;
-    let away = 0;
-    for (let i = 0; i < n; i++) {
-      if (pointer == i) {
-        continue;
-      } else {
-        if (teams[pointer][1] == teams[i][0]) {
-          home++;
-        } else {
-          away++;
-        }
-      }
+  var rows = n;
+var columns = 2;
+
+// Create a 2D array with inner arrays pre-filled with 0, 0
+var count = new Array(rows);
+
+for (var i = 0; i < rows; i++) {
+  count[i] = new Array(columns).fill(0);
+}
+
+let pointer = 0;
+let home = n - 1;
+while (pointer < n) {
+  for (let i = pointer + 1; i < n; i++) {
+    if (teams[pointer][1] == teams[i][0]) {
+      count[pointer][0]++;
+    } else {
+      count[pointer][1]++;
     }
-    console.log(`${home} ${away}`)
-    pointer++;
+
+    if (teams[pointer][0] == teams[i][1]) {
+      count[i][0]++;
+    } else {
+      count[i][1]++;
+    }
   }
+  count[pointer][0] = home + count[pointer][0];
+  console.log(`${count[pointer][0]} ${count[pointer][1]}`);
+  pointer++;
+ 
+}
 
 }
