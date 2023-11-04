d6c197d0b65510c3ce9c8e80ca17b9ce3193abe1
unknown <sayooj0076@gmail.com>
2023-11-04 18:18:40 +0530

    6:18

--- Solutions/CodeForces_Div_2_B/268_Div_2.js
+++ Solutions/CodeForces_Div_2_B/268_Div_2.js
@@ -16,13 +16,15 @@ while (l <= r) {
   let flag = false;
   for (let j = 0; j < qCount; j++) {
     for (let i = 0; i < pCount; i++) {
-      if (p[i][0] <= q[j][0] + l || p[i][1] >= q[j][1] + l) {
+      if (
+        p[i][0] <= q[j][0] + l <= p[i][1] &&
+        p[i][0] <= q[j][1] + l <= p[i][1]
+      ) {
         console.log(l);
         flag = true;
         break;
       }
     }
-  
   }
   if (flag) {
     count++;
